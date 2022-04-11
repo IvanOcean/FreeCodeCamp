82699c8028cd0a109d4048130f72abed181eeeeb
Ivan Ocean <aprx1994@mail.ru>
2022-04-11 22:21:36 +0700

    completed new lessons

---
+++ Array.js
@@ -0,0 +1,25 @@
+// Only change code below this line
+const myArray = ["Hello", 2];
+// Only change code below this line
+const myArray = [["Hello", 2], ["world", 1]];
+
+const myArray = [50, 60, 70];
+var myData = myArray[0];
+console.log(myData);
+
+// Setup
+const myArray = [18, 64, 99];
+// Only change code below this line
+myArray[0] = 45;
+
+const myArray = [
+  [1, 2, 3],
+  [4, 5, 6],
+  [7, 8, 9],
+  [[10, 11, 12], 13, 14]];
+const myData = myArray[2][1]; //8
+
+// Setup
+const myArray = [["John", 23], ["cat", 2]];
+// Only change code below this line
+myArray.push(["dog", 3]);
---
+++ BracketNotation.js
@@ -0,0 +1,20 @@
+// Setup
+let firstLetterOfLastName = "";
+const lastName = "Lovelace";
+// Only change code below this line
+firstLetterOfLastName = lastName[0]; // Change this line
+
+// Setup
+const lastName = "Lovelace";
+// Only change code below this line
+const thirdLetterOfLastName = lastName[2]; // Change this line
+
+// Setup
+const lastName = "Lovelace";
+// Only change code below this line
+const lastLetterOfLastName = lastName[lastName.length - 1]; // Change this line
+
+// Setup
+const lastName = "Lovelace";
+// Only change code below this line
+const secondToLastLetterOfLastName = lastName[lastName.length - 2]; // Change this line
